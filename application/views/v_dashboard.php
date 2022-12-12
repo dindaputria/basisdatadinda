c6acd94ca9fa8c7ebccee36792aed83d51bbf01a
dindaputria <dindaputrisyah190914@gmail..com>
2022-12-12 08:19:21 +0700

    dinda

---
+++ application/views/v_dashboard.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="jumbotron">
+        <h1 class="display-4">KLINIK IBUKU</h1>
+        <p class="lead">Klinik Andalan Keluarga Anda</p>
+        <hr class="my-4">
+    </div>
+    <br><br>
+    <img src="<?= base_url('assets/ds.jpg') ?>" alt="">
+</div>
